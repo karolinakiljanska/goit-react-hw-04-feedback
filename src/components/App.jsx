e8e32ed6672f8b613251a0ea7b32c2b92e82cb16
karolinakiljanska <karolinakiljanska112@gmail.com>
2023-09-19 00:31:01 +0200

    zadanie

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,128 @@
-export const App = () => {
+import React, { useState } from 'react';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Notification } from './Notification/Notification';
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onLeaveFeedback = value => {
+    switch (value) {
+      case 'good':
+        setGood(prev => prev + 1);
+        break;
+      case 'neutral':
+        setNeutral(prev => prev + 1);
+        break;
+      case 'bad':
+        setBad(prev => prev + 1);
+        break;
+
+      default:
+        throw new Error();
+    }
+  };
+
+  function countTotalFeedback() {
+    return good + neutral + bad;
+  }
+
+  function countPositiveFeedbackPercentage() {
+    return Math.round((good / countTotalFeedback()) * 100);
+  }
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={CSS.container}>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys({ good, neutral, bad })}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
     </div>
   );
-};
+}
+
+// class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+//   onLeaveFeedback = state => {
+//     this.setState(prevState => ({
+//       [state]: prevState[state] + 1,
+//     }));
+//   };
+
+//   countTotalFeedback() {
+//     const { good, neutral, bad } = this.state;
+//     return good + neutral + bad;
+//   }
+//   countPositiveFeedbackPercentage() {
+//     const { good } = this.state;
+//     return Math.round((good / this.countTotalFeedback()) * 100);
+//   }
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     const options = Object.keys(this.state);
+
+//     return (
+//       <div className={CSS.container}>
+//         <Section title="Please leave feedback">
+//           <FeedbackOptions
+//             options={options}
+//             onLeaveFeedback={this.onLeaveFeedback}
+//           />
+//         </Section>
+//         <Section title="Statistics">
+//           {this.countTotalFeedback() > 0 ? (
+//             <Statistics
+//               good={good}
+//               neutral={neutral}
+//               bad={bad}
+//               total={this.countTotalFeedback()}
+//               positivePercentage={this.countPositiveFeedbackPercentage()}
+//             />
+//           ) : (
+//             <Notification message="There is no feedback" />
+//           )}
+//         </Section>
+//       </div>
+//     );
+//   }
+// }
+
+export default App;
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
