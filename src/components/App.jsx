8b10a13741c0fead07e9d191fa77e73539570936
karolinakiljanska <karolinakiljanska112@gmail.com>
2023-09-21 20:14:22 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,50 +4,45 @@ import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 
-function App() {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+const App = () => {
+  const [stats, setStats] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
 
-  const onLeaveFeedback = value => {
-    switch (value) {
-      case 'good':
-        setGood(prev => prev + 1);
-        break;
-      case 'neutral':
-        setNeutral(prev => prev + 1);
-        break;
-      case 'bad':
-        setBad(prev => prev + 1);
-        break;
-
-      default:
-        throw new Error();
-    }
+  const onLeaveFeedback = name => {
+    setStats(prevState => {
+      const value = prevState[name];
+      return {
+        ...prevState,
+        [name]: value + 1,
+      };
+    });
   };
 
   function countTotalFeedback() {
-    return good + neutral + bad;
+    return stats.good + stats.neutral + stats.bad;
   }
 
   function countPositiveFeedbackPercentage() {
-    return Math.round((good / countTotalFeedback()) * 100);
+    return Math.round((stats.good / countTotalFeedback()) * 100);
   }
 
   return (
     <div className={CSS.container}>
       <Section title="Please leave feedback">
         <FeedbackOptions
-          options={Object.keys({ good, neutral, bad })}
+          options={Object.keys(stats)}
           onLeaveFeedback={onLeaveFeedback}
         />
       </Section>
       <Section title="Statistics">
         {countTotalFeedback() > 0 ? (
           <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
+            good={stats.good}
+            neutral={stats.neutral}
+            bad={stats.bad}
             total={countTotalFeedback()}
             positivePercentage={countPositiveFeedbackPercentage()}
           />
@@ -57,6 +52,6 @@ function App() {
       </Section>
     </div>
   );
-}
+};
 
 export default App;
